c21a13fe0abb8b277af6075c08048ef53e060ed5
mugishajc_rw <37206113+mugishajc@users.noreply.github.com>
2021-06-03 10:04:46 +0200

    n

--- app/Motari.php
+++ app/Motari.php
@@ -6,12 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Motari extends Model
 {
-<<<<<<< Updated upstream
-    protected $table = "motari";
-=======
+
     // 
      protected $table = "motari";
->>>>>>> Stashed changes
     protected $fillable = [
         'NAMES','COOPERATIVE','Ref_Number','AMOUNTRWF','ID_NUMBER','AC_NUMBER','PLATE_NUMBER','VILLAGE','CELL','SECTOR','TELEPHONE','DEVICE_IMEI',
     ];
